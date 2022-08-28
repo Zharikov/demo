e358ba3d8e405470ddd95a6bfecf6def4ed51d2a
zharikov <a.zharikov@ase-ec.ru>
2022-08-29 02:15:33 +0300

    read.me update

--- src/main/java/com/example/demo/service/BookServiceImpl.java
+++ src/main/java/com/example/demo/service/BookServiceImpl.java
@@ -3,9 +3,9 @@ package com.example.demo.service;
 import com.example.demo.entity.BookEntity;
 import com.example.demo.entity.BookRepository;
 import com.example.demo.to.BookTO;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.TreeMap;
@@ -23,9 +23,17 @@ public class BookServiceImpl extends CommonServiceImpl<BookEntity, BookTO, Long,
     }
 
     @Override
+    @Cacheable(value = "books", key = "#character")
     public List<BookTO> getAllByLetter(Character character) {
         return getAll().stream()
-                .filter(x ->  x.getName().toLowerCase(Locale.ROOT).charAt(0) == character)
+                .filter(x -> x.getName().toLowerCase(Locale.ROOT).charAt(0) == character)
                 .collect(Collectors.toList());
     }
+
+    @Override
+    @Cacheable("books")
+    public List<BookTO> getAll() {
+        return super.getAll();
+    }
+
 }
